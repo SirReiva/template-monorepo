580d1664213686a2be5aaa5880cb218a093f0990
SirReiva <gjjavicoru@gmail.com>
2024-12-11 21:23:25 +0100

    move sourcemap

--- loader/index.mjs
+++ loader/index.mjs
@@ -2,6 +2,11 @@
 import { register } from "node:module";
 import { sep } from "node:path";
 import { pathToFileURL } from "node:url";
+import { install } from "source-map-support";
+
+install({
+	environment: "node",
+});
 
 const packageName =
 	process.argv[1]
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,7 @@
 import { serve } from "@hono/node-server";
 import { port } from "@template/common";
 import { showRoutes } from "hono/dev";
-import { install } from "source-map-support";
 import { app } from "./app";
-
-install({
-	environment: "node",
-});
 
 serve({
 	fetch: app.fetch,

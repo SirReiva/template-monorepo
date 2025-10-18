95c2a9f0661df083706de4291340b61d51b65f9f
SirReiva <gjjavicoru@gmail.com>
2025-10-18 22:50:42 +0200

    feat: add loader monorepo plugin to Vite configuration

---
+++ loader/vite.plugin.ts
@@ -0,0 +1,22 @@
+import figlet from "figlet";
+import { rainbow } from 'gradient-string';
+import { basename, resolve } from "path";
+import { cwd } from "process";
+import type { Plugin } from "vite";
+import packageJson from "../package.json" with { type: "json" };
+
+const workspaceName = `@${packageJson.name}`;
+const version = packageJson.version;
+
+const packageName = basename(
+	cwd().replace(resolve(import.meta.dirname, "../packages"), "")
+);
+
+export const loaderMonorepoPlugin: Plugin = {
+	name: "loader-monorepo",
+	config() {
+		console.clear();
+		figlet(`${workspaceName}/${packageName}#${version}`, (_, text = '') => console.log(rainbow.multiline(text)));
+			console.log(`NodeJS${process.version}`);
+	},
+};
--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -2,9 +2,10 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { loaderMonorepoPlugin } from "../../loader/vite.plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tsconfigPaths(), react()],
+	plugins: [loaderMonorepoPlugin, tsconfigPaths(), react()],
 	cacheDir: resolve(import.meta.dirname, "../../.cache/front"),
 });
